d17342e7e1c2e60daaf772468ec0ab96fcfdee64
shodikulovGithub <62528391+shodikulovGithub@users.noreply.github.com>
2023-01-10 22:54:21 -0500

    Dealer and Person files

---
+++ Dealer.java
@@ -0,0 +1,23 @@
+package boaFinalProject;
+
+ //Handles all Dealer specific operations
+public class Dealer extends Person{
+
+      //Create a new Dealer
+    public Dealer(){
+
+        //Name the dealer "Dealer"
+        super.setName("Dealer");
+
+    }
+
+    //Prints the dealer's first hand, with one card face down.
+    public void printFirstHand(){
+        System.out.println("The dealer's hand:");
+        System.out.println(super.getHand().getCard(0));
+        System.out.println("The second card is face down.");
+    }
+
+
+
+}
