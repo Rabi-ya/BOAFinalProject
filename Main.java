5ad1b7fc9541baf88e068ea64b12e7700f3049ac
Rabi-ya <97138269+Rabi-ya@users.noreply.github.com>
2023-01-10 20:28:26 -0500

    Main Class

---
+++ Main.java
@@ -0,0 +1,13 @@
+package JAVAProjects.boAFinalProject;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Blackjack");
+
+        //Create and start the Game
+        Game blackjack = new Game();
+
+
+    }
+}
