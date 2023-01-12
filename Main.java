d7ddadacf14aa6f85b81ae2b73969875fb75ff38
shodikulovGithub <62528391+shodikulovGithub@users.noreply.github.com>
2023-01-11 19:54:07 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package JAVAProjects.boAFinalProject;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Blackjack: Rule you have to get to get higher count than the dealer but it has not to be more than 21. You can Hit by placing 1 or stand by placing 2!");
+        System.out.println("Welcome to Blackjack. Rule for this game: you have to get to get higher count than the dealer but it has not to be more than 21. You can Hit by placing 1 or stand by placing 2!");
 
         //Create and start the Game
         Game blackjack = new Game();
