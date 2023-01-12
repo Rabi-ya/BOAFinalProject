d282b8b38dd1fbed5f24cf8bb43fb5e5886bb260
Jatin930 <104586736+Jatin930@users.noreply.github.com>
2023-01-12 00:34:05 -0500

    Updated Game Rules

--- Main.java
+++ Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Blackjack. Rule for this game:\n" +
-                "- Players start with two cards and have an option to hit (draw another card) or stand (end their turn) \n" +
+                "********************************************************************************************************************************************************** \n" +
+
+        "- Players start with two cards and have an option to hit (draw another card) or stand (end their turn) \n" +
                 "- Number cards are valued at their number, Face cards (Jack, Queen, and King) are valued at 10, and aces are valued at 1 or 11 depending on the situation \n" +
                 "- If the value of the hand with an ace puts it over 21, the ace is valued at 1. If it's doesn't, it's 11. \n" +
                 "  So, for example, a King and an ace would be 21, but one King, a 9, and an Ace would be valued at 20 (10+9+1=20). \n" +
@@ -13,7 +15,8 @@ public class Main {
                 "- The player may stand (stop drawing) at any time. If they go over 21 they bust and lose.   \n" +
                 "  When the player stands, it's the end of their turn, and the dealer begins drawing their third, fourth, fifth card, and so on.  \n" +
                 "- If neither player nor dealer busts or gets BlackJack, the player with the highest score wins the round.\n" +
-                "- The dealer will keep drawing cards until they reach a hand valued at 17 or higher.    ");
+                "- The dealer will keep drawing cards until they reach a hand valued at 17 or higher.    \n" +
+        "**********************************************************************************************************************************************************");
 
         //Create and start the Game
         Game blackjack = new Game();
